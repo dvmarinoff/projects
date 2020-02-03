cbcfc43b44065b6169b1100c50c13a99a1145e93
dvmarinoff <dvmarinoff@gmail.com>
2020-02-04 00:24:02 +0200

    first version

---
+++ js/main.js
@@ -0,0 +1,6 @@
+;(function () {
+    'use strict';
+
+    console.log("Hello, those are a couple of projects I've done in the past.\nSince it seems you know how to use the dev tools you can check out this code\nto get some idea of my style: https://github.com/dvmarinoff/jquery_starter_kit/blob/master/public/js/main.js \nit is used in every of those projects.");
+
+}());
